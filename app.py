c5402a325fd124a22f4b5f1f8870dad540844531
Gustavo Di Domenico <gustavo.domenico@transferwise.com>
2020-03-23 19:00:05 +0000

    Add ablity to download files.

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask import Flask
 from controllers import admin
 from controllers import protocols
 from controllers import uploads
+from controllers import downloads
 
 app = Flask(__name__)
 
 app.register_blueprint(admin.api)
 app.register_blueprint(protocols.api)
 app.register_blueprint(uploads.api)
+app.register_blueprint(downloads.api)
 
 if __name__ == '__main__':
     app.run(debug=True)
